cd1ad5537afa9174bd613eb1fa8a6c436a761dfe
rihaan61 <rihanbhs19@gmail.com>
2025-10-31 22:58:21 +0530

    Add files via upload

---
+++ Constructorexdemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Constructorexdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            democons.say();
+        }
+    }
+}
---
+++ Constructorexdemo/democons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructorexdemo
+{
+    public class democons
+    {
+        static int age;
+
+        static  democons()
+        {
+            age = 28;
+            Console.WriteLine("Hello");
+        }
+        public  static void say()
+        {
+            Console.WriteLine("Age="+age);
+        }
+    }
+}
