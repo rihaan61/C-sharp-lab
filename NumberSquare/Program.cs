8b425e129b3f9a2239386e9d0fb3764b76db301d
rihaan61 <rihanbhs19@gmail.com>
2025-10-31 22:59:39 +0530

    Add files via upload

---
+++ NumberSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NumberSquare
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Squarecalculator c = new Squarecalculator();
+            int square = c.func1();
+            Console.WriteLine(" Square of  "    + c.n +   "  is  "   + square);
+        }
+    }
+}
---
+++ NumberSquare/Squarecalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSquare
+{
+    public class Squarecalculator
+    {
+        public int n = 6;
+        public int func1()
+        {
+            return n * n;
+        }
+    }
+}
