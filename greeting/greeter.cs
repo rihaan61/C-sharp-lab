be466d1b010660ca88537288b64bcf6821b0834b
rihaan61 <rihanbhs19@gmail.com>
2025-10-31 22:58:32 +0530

    Add files via upload

---
+++ greeting/greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greeting
+{
+    public class greeter
+    {
+        public string name = "prajwal";
+        public void greet()
+        {
+                       Console.WriteLine($"Hello, {name} welcome!");
+        }
+    }
+}
