851cb599087d29745b4289f2608d58f479278e3a
rihaan61 <rihanbhs19@gmail.com>
2025-10-31 22:57:02 +0530

    Add files via upload

---
+++ positiveorneg/NumberCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace positiveorneg
+{
+    internal class NumberCheck
+    {
+        public int n = 8;
+        public void Checknumb()
+        {
+            if (n > 0)
+            {
+                Console.WriteLine("The number is positive");
+                
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine("The number is negative");
+                
+            }
+            else
+            {
+                Console.WriteLine("The number is zero");
+                 
+            }
+        }
+    }
+}
---
+++ positiveorneg/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace positiveorneg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberCheck numberCheck = new NumberCheck();
+            numberCheck.Checknumb();
+        }
+    }
+}
