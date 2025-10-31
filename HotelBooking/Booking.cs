64c78b58a8a266a5a9a1c447de64690d38e7d3b1
rihaan61 <rihanbhs19@gmail.com>
2025-10-31 22:59:10 +0530

    Add files via upload

---
+++ HotelBooking/Booking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking
+{
+    public class Booking
+    {
+        public string service;
+        public int days;
+        private string guestname;
+        public Booking(string name)
+        {
+            string guestname = name;
+            int days = 3;
+            Console.WriteLine($"guest name:{guestname} for  {days} days");
+        }
+        public void Book(string name, int days)
+        {
+            this.days = days;
+            Console.WriteLine($"new guest:{name} for {days} days");
+        }
+        public void  Hotel(string name,int days,string service)
+        {
+            this.service = service;
+            Console.WriteLine($"guest named {name} asked service for {days} days Service::{service} ");
+
+        }
+    }
+}
---
+++ HotelBooking/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelBooking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-----------HOTEL CUSTOMER DETAILS--------------");
+            Booking b = new Booking("Rajat");
+            Booking b1 = new Booking("Jitesh");
+            Booking b2 = new Booking("Romario");
+            b.Book("tim",9);
+            b.Hotel("Jitesh", 1, "Spa");
+        }
+    }
+}
